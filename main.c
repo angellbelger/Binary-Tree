0d3fdc4dbda25602edb46d55b69b06ccfdb41435
Angell Belger <angellbelger@hotmail.com>
2022-07-08 17:25:51 -0300

    update

--- main.c
+++ main.c
@@ -1,58 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node
+struct no
 {
-    int key;
-    struct node *left;
-    struct node *right;
+    int chave;
+    struct no *esquerda;
+    struct no *direita;
 };
 
 //retorna um novo nó com o valor dado
-struct node *getNode(int val)
+struct no *getNo(int val)
 {
-    struct node *newNode;
+    struct no *novoNo;
 
-    newNode = malloc(sizeof(struct node));
+    novoNo = malloc(sizeof(struct no));
 
-    newNode->key   = val;
-    newNode->left  = NULL;
-    newNode->right = NULL;
+    novoNo->chave = val;
+    novoNo->esquerda  = NULL;
+    novoNo->direita = NULL;
 
-    return newNode;
+    return novoNo;
 }
 
 //insere nós na árvore de busca binária
-struct node *insertNode(struct node *root, int val)
+struct no *insertNo(struct no *root, int val)
 {
      if(root == NULL)
-         return getNode(val);
+         return getNo(val);
 
-     if(root->key < val)
-         root->right = insertNode(root->right,val);
+     if(root->chave < val)
+         root->direita = insertNo(root->direita,val);
 
-     if(root->key > val)
-         root->left = insertNode(root->left,val);
+     if(root->chave > val)
+         root->esquerda = insertNo(root->esquerda,val);
 
      return root;
 }
 
 //percurso de pré-ordem da árvore binária
-void preorder(struct node *root)
+void preordem(struct no *root)
 {
     if(root == NULL)
         return;
 
-    printf("%d ",root->key);
+    printf("%d ",root->chave);
 
-    preorder(root->left);
+    preordem(root->esquerda);
 
-    preorder(root->right);
+    preordem(root->direita);
 }
 
 int main()
 {
-   struct node *root = NULL;
+   struct no *root = NULL;
    int data;
    char option;
 
@@ -63,14 +63,14 @@ int main()
        printf("\n---------------------------\n");
        printf("\nDigite um numero: ");
        scanf("%i", &data);
-       root = insertNode(root, data);
+       root = insertNo(root, data);
        printf("\nDigite \033[36m'Y'\033[m para continuar ou \033[31m'N'\033[m para encerrar e ver o resultado.");
        printf("\nDigite: [ \033[36mY\033[34m\033[m | \033[31mN\033[34m\033[m ]: ");
        scanf(" %c", &option);
    } while ( option != 'n');
 
     printf("\nResultado em Pre-ordem:\033[36m ");
-    preorder(root);
+    preordem(root);
     printf("\n\033[m\033[31mPrograma finalizado.\033[m\n"); 
 
    return 0;
